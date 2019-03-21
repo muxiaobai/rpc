5588ee5bde4f29dd628cc508a9387527350f0187
muxiaobai <zpf12345678910@gmail.com>
2019-03-21 19:35:22 +0800

    remote

---
+++ spring-boot/src/main/java/io/github/muxiaobai/spring_boot/service/DemoService.java
@@ -0,0 +1,34 @@
+/**
+ * Project Name:spring-boot
+ * File Name:DemoService.java
+ * Package Name:io.github.muxiaobai.spring_boot.service
+ * Date:2019年3月21日下午7:31:58
+ * Copyright (c) 2019, All Rights Reserved.
+ *
+*/
+
+package io.github.muxiaobai.spring_boot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * ClassName:DemoService 
+ * Function: TODO 
+ * Reason:	 TODO 
+ * Date:     2019年3月21日 下午7:31:58 
+ * @author   Mu Xiaobai
+ * @version  
+ * @since    JDK 1.8	 
+ */
+
+@Service
+public class DemoService {
+    @Autowired
+    private RemoteCall remoteCall;
+    
+    public void demoRemote(String code){
+//        remoteCall
+    }
+}
+
---
+++ spring-boot/src/main/java/io/github/muxiaobai/spring_boot/service/RemoteCall.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:spring-boot
+ * File Name:RemoteCall.java
+ * Package Name:io.github.muxiaobai.spring_boot.service
+ * Date:2019年3月21日下午7:32:47
+ * Copyright (c) 2019, All Rights Reserved.
+ *
+*/
+
+package io.github.muxiaobai.spring_boot.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * ClassName:RemoteCall 
+ * Function: TODO 
+ * Reason:	 TODO 
+ * Date:     2019年3月21日 下午7:32:47 
+ * @author   Mu Xiaobai
+ * @version  
+ * @since    JDK 1.8	 
+ */
+@Service
+public class RemoteCall {
+
+}
+
