294a1a78e12941ed0bd59d9c0de175d678a5a7f0
muxiaobai <zpf12345678910@gmail.com>
2019-03-21 19:25:29 +0800

    spring-boot 使用多线程远程调用，合并处理

---
+++ spring-boot/src/main/java/io/github/muxiaobai/spring_boot/Application.java
@@ -0,0 +1,35 @@
+/**
+ * Project Name:spring-boot
+ * File Name:Application.java
+ * Package Name:io.github.muxiaobai.spring_boot
+ * Date:2019年3月21日下午7:13:52
+ * Copyright (c) 2019, All Rights Reserved.
+ *
+*/
+
+package io.github.muxiaobai.spring_boot;
+/**
+ * ClassName:Application 
+ * Function: TODO 
+ * Reason:	 TODO 
+ * Date:     2019年3月21日 下午7:13:52 
+ * @author   Mu Xiaobai
+ * @version  
+ * @since    JDK 1.8	 
+ */
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+      public static void main(String[] args) {
+          SpringApplication.run(Application.class, args);
+      }
+      
+}
