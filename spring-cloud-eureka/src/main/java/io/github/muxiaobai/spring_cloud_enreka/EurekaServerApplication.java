1cad7c8acd9f2f9bba2965a3a007c9f6c6fde577
muxiaobai <muxiaobai123456>
2019-05-29 17:21:42 +0800

    spring-cloud-eureka

--- spring-cloud-eureka/src/main/java/io/github/muxiaobai/spring_cloud_enreka/EurekaServerApplication.java
+++ spring-cloud-eureka/src/main/java/io/github/muxiaobai/spring_cloud_enreka/EurekaServerApplication.java
@@ -12,12 +12,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
       public static void main(String[] args) {
-          SpringApplication.run(EurekaServerApplication.class, args);
-//          new SpringApplicationBuilder(EurekaServerApplication.class).web(WebApplicationType.NONE).run(args);
+         SpringApplication.run(EurekaServerApplication.class, args);
       }
       
 }
