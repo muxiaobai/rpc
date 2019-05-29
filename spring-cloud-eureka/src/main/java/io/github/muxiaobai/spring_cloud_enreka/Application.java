f7058a67c05bfe40053911ba593098f7830344a9
muxiaobai <muxiaobai123456>
2019-05-29 15:09:08 +0800

    spring-cloud-enreka

---
+++ spring-cloud-eureka/src/main/java/io/github/muxiaobai/spring_cloud_enreka/Application.java
@@ -0,0 +1,19 @@
+package io.github.muxiaobai.spring_cloud_enreka; /**
+ * Project Name:spring-boot
+ * File Name:Application.java
+ * Package Name:io.github.muxiaobai.spring_boot
+ * Date:2019年3月21日下午7:13:52
+ * Copyright (c) 2019, All Rights Reserved.
+ *
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+      public static void main(String[] args) {
+          SpringApplication.run(Application.class, args);
+      }
+      
+}
