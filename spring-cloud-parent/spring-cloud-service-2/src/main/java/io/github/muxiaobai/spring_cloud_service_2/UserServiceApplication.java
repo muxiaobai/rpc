9f501391b2ab636d9260f25f404089d4649d69cf
muxiaobai <muxiaobai123456>
2020-08-13 20:17:12 +0800

    仓库

--- spring-cloud-parent/spring-cloud-service-2/src/main/java/io/github/muxiaobai/spring_cloud_service_2/UserServiceApplication.java
+++ spring-cloud-parent/spring-cloud-service-2/src/main/java/io/github/muxiaobai/spring_cloud_service_2/UserServiceApplication.java
@@ -10,9 +10,11 @@ package io.github.muxiaobai.spring_cloud_service_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
-//@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
       public static void main(String[] args) {
